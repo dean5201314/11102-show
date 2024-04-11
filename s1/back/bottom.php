35904e6cd05ea9458d9c0132f02b076a25e56f11
dean5201314 <smile90624@gmail.com>
2024-04-12 01:24:49 +0800

    site1: 簡化total和bottom的api程式,並在前端表單增加一個table欄位

--- s1/back/bottom.php
+++ s1/back/bottom.php
@@ -1,12 +1,15 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">頁尾版權資料管理</p>
     <!-- 因為被include到back.php，所以action要以back.php位置找edit_bottom.php  -->
-    <form method="post" action="./api/edit_bottom.php">
+    <form method="post" action="./api/edit_info.php">
         <table style="width:50%; margin:auto;">
             <tbody>
                 <tr class="yel">
                     <td width="50%">頁尾版權資料</td>
-                    <td width="50%"><input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>"></td>
+                    <td width="50%">
+                        <input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>">
+                        <input type="hidden" name="table" value="bottom">
+                    </td>
                 </tr>
             </tbody>
         </table>
