a660aed87e1c1dadff185ea1eb122c024bbc1f96
dean5201314 <smile90624@gmail.com>
2024-04-14 16:52:31 +0800

    site1: 完成標題圖片編輯功能

--- s1/back/title.php
+++ s1/back/title.php
@@ -1,6 +1,7 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">網站標題管理</p>
-    <form method="post" action="?do=tii">
+    <!-- 因為被include到back.php，所以action要以back.php位置找edit.php  -->
+    <form method="post" action="./api/edit.php">
         <table width="100%" style="text-align: center;">
             <tbody>
                 <tr class="yel">
@@ -24,10 +25,11 @@
                         style="width:90%" value="<?=$row['text'];?>">
                     </td>
                     <td width="7%">
-                        <input type="radio" name="sh" id="<?=$row['id'];?>">
+                        <input type="radio" name="sh" value="<?=$row['id'];?>" 
+                        <?=($row['sh']==1)?'checked':'';?>>
                     </td>
                     <td width="7%">
-                        <input type="checkbox" name="del[]" id="<?=$row['id'];?>">
+                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
                     </td>
                     <td>
                     <input type="button" onclick="op('#cover','#cvr','./modal/update.php?table=<?=$do?>&id=<?=$row['id'];?>')" value="更新圖片">
