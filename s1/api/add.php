9461117003a5bd3bc8afbe3bd8f2468d34abcac1
dean5201314 <smile90624@gmail.com>
2024-04-25 19:11:00 +0800

    site1: 修正add.php讓admin功能也能使用

--- s1/api/add.php
+++ s1/api/add.php
@@ -3,14 +3,21 @@ include_once "./db.php";
 
 $DB=${ucfirst($_POST['table'])};
 $table=$_POST['table'];
-
+// 若為admin在呼叫，則移除$_POST中資料表不存在的變數pw2
+switch ($table) {
+    case 'admin':
+        unset($_POST['pw2']);
+    break;
+}
 if (isset($_FILES['img']['tmp_name'])) {
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
-
-$_POST['sh']=($table=='title')?0:1;
-
+// 若非admin呼叫，則設定顯示欄位sh中的預設值(只有admin資料表沒有sh欄位)
+if ($table != 'admin') {
+    $_POST['sh']=($table=='title')?0:1;
+}
+// 移除$_POST中資料表不存在的變數table
 unset($_POST['table']);
 $DB->save($_POST);
 
