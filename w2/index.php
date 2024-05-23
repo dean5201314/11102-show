166ee0a561ec1b97ee2ebd4c39cacfc9ddd86056
dean5201314 <smile90624@gmail.com>
2024-05-24 01:57:05 +0800

    w2: 將縱向(欄)的變化的規則，寫程式用第二個迴圈呈現在網頁上

--- w2/index.php
+++ w2/index.php
@@ -31,36 +31,12 @@
         <br>
 
         <?php
-        for($i = 1 ; $i <= 9 ; $i++){
-            echo "1 x " . $i . " = " . (1*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
+        for($j=1;$j<=9;$j++){
+            for($i=1;$i<=9;$i++){        
+                echo $j . " x " . $i . " = " . ($j*$i) . " ,&nbsp;&nbsp;&nbsp;&nbsp; ";        
+            }
         echo "<br>";
-        for($i = 1 ; $i <= 9 ; $i++){
-            echo "2 x " . $i . " = " . (2*$i) . " ,&nbsp;&nbsp;&nbsp; ";
         }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "3 x " . $i . " = " . (3*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "4 x " . $i . " = " . (4*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "2 x " . $i . " = " . (5*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "6 x " . $i . " = " . (6*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "7 x " . $i . " = " . (7*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";for($i = 1 ; $i <= 9 ; $i++){
-            echo "8 x " . $i . " = " . (8*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";
-        for($i = 1 ; $i <= 9 ; $i++){
-            echo "9 x " . $i . " = " . (9*$i) . " ,&nbsp;&nbsp;&nbsp; ";
-        }
-        echo "<br>";
         ?>
 
     </div>
