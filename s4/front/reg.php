a81d7c30401c0cd6b09640a629d166c3e1b81528
dean5201314 <smile90624@gmail.com>
2024-06-05 18:49:34 +0800

    site4: 完成註冊帳號檢查及新增會員帳號功能

--- s4/front/reg.php
+++ s4/front/reg.php
@@ -31,21 +31,55 @@
 </table>
 <!-- .ct>btn*2 -->
 <div class="ct">
-    <button>註冊</button>
-    <button>重置</button>
+    <button onclick="reg()">註冊</button>
+    <button onclick="clean()">重置</button>
 </div>
 <script>
+
 function chkacc(){
     let acc=$("#acc").val()
     /* $.get()是系統函數，呼叫程式:"./api/chk_acc.php"，傳入參數:{acc}，
        程式回傳值:(res)，收到回傳值後要做的事(回應呼叫的函數): =>{} (簡寫表示法) */
     $.get("./api/chk_acc.php",{acc},(res)=>{
         /* parseInt(res):將回傳值(res)強制轉為數值的資料型態 */
-        if (parseInt(res)==1) {
+        if (parseInt(res)==1 || acc=='admin') {
             alert(`此帳號${acc}已被使用`)
         } else {
             alert(`此帳號${acc}可以使用`)
         }
     })
 }
+
+function reg(){
+    /* 將所有input元件的值，組合成一個dataset集合，作為傳入的參數 */
+    let user={
+        name:$("#name").val(),
+        acc:$("#acc").val(),
+        pw:$("#pw").val(),
+        tel:$("#tel").val(),
+        addr:$("#addr").val(),
+        email:$("#email").val()
+    }
+
+    /* 因為用ajax非同步驗證帳號，故不知程式回傳值(res)何時才收到(除非用 promise 架構)，
+       故將帳號檢核功能，直接重做一次，但傳入參數需改格式為{acc:user.acc}(因改用dataset) */
+    $.get("./api/chk_acc.php",{acc:user.acc},(res)=>{
+        /* parseInt(res):將回傳值(res)強制轉為數值的資料型態 */
+        if (parseInt(res)==1 || user.acc=='admin') {
+            alert(`此帳號${user.acc}已被使用`)
+        } else {
+            // 因包含用戶帳密，故用$.post()方式傳值較安全
+            $.post("./api/reg.php",user,(res)=>{
+                // 註冊是為登入網站，故成功後重新導向到登入頁面
+                location.href='?do=login'
+            })
+        }
+    })
+}
+
+/* 因reset為系統功能，故用clean()為名稱，避免衝到reset造成問題 */
+function clean(){
+    $("#name,#acc,#pw,#tel,#addr,#email").val('')
+}
+
 </script>
