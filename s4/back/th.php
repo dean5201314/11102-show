eb4e294b8576611de9a2e6b9ff81eb3b9be7e53d
dean5201314 <smile90624@gmail.com>
2024-06-22 23:59:16 +0800

    site4: 使用ajax 來完成修改分類功能

--- s4/back/th.php
+++ s4/back/th.php
@@ -31,6 +31,7 @@ foreach ($bigs as $big) {
         </td>
     </tr>
 <?php
+// 實務上宜先用if()判斷是否有中分類，若有資料再讀取中分類資料，為縮短時間故省略
     // 讀取中分類的內容(big_id=大分類的代碼)
     $mids=$Type->all(['big_id'=>$big['id']]);
     // 將每筆中分類資料放入每一列中分類的HTML內容中
@@ -55,13 +56,28 @@ foreach ($bigs as $big) {
 <script>
 // 呼叫getTypes()，讀取所有大分類(big_id=0)下一階的中分類
 getTypes(0);
-// 呼叫
+// 呼叫getTypes()，依大分類(big_id)讀取其下所有下一階的中分類
 function getTypes(big_id){
     // 呼叫"./api/get_types.php"程式，讀取中分類並產生選項內容的HTML碼
     $.get("./api/get_types.php",{big_id},(types)=>{
         // 用types回傳選項的HTML內容，放入id=bigs的HTML標籤容器內(bigs在此為select選項標籤)
         $("#bigs").html(types);
     })
+}
+// 利用prompt彈跳視窗特性(確認回傳修改後的資料，取消回傳null空值)，修改分類名稱的功能
+function edit(dom,id){
+    // 取出dom(buttom)的父物件(td)的前一物件(name的表單欄位)之text屬性內容，放入name變數中
+    let name=prompt("請輸入您要修改的分類名稱：",`${$(dom).parent().prev().text()}`)
+    // 若prompt視窗回傳不是null空值，則將資料內容更新回資料表
+    if (name!=null) {
+        // 用ajax的post方式呼叫"./api/save_type.php"程式，傳入{name,id}，寫入資料表
+        $.post("./api/save_type.php",{name,id},()=>{
+            // 利用JQ功能，更新dom(buttom)的父物件(td)的前一物件(name的表單欄位)之text屬性內容，不觸發全網頁更新
+            $(dom).parent().prev().text(name);
+            // 若以下述指令重新載入頁面，將觸發全網頁更新，耗時較久
+            // location.reload();
+        })
+    }
 }
 
 // 依type內容新增大分類(big)或中分類(mid)的內容
@@ -82,7 +98,7 @@ function addType(type){
         break;
     }
 
-    // 用ajax方式呼叫"./api/save_type.php"程式，傳入{name,big_id}，寫入資料表
+    // 用ajax的post方式呼叫"./api/save_type.php"程式，傳入{name,big_id}，寫入資料表
     $.post("./api/save_type.php",{name,big_id},()=>{
         // 重新載入頁面，以顯示新增的大分類或中分類內容
         location.reload();
