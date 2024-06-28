d9396604add5e88d58b81d81ceb772581fb5b6db
dean5201314 <smile90624@gmail.com>
2024-06-29 00:16:09 +0800

    site4: 完成後台商品修改功能

--- s4/api/get_types.php
+++ s4/api/get_types.php
@@ -5,6 +5,7 @@ include_once "db.php";
 // 可將上述程式碼簡化成下一行，因$_GET陣列結構與all()內所需參數陣列結構相同，只有一個名稱為'big_id'的元素值
 $types=$Type->all($_GET);
 foreach ($types as $type) {
+    // 製作出商品(大/中)分類碼的選項值與名稱的HTML程式碼
     echo "<option value='{$type['id']}'>{$type['name']}</option>";
 }
 ?>
--- s4/api/save_goods.php
+++ s4/api/save_goods.php
@@ -6,12 +6,15 @@ if (!empty($_FILES['img']['tmp_name'])) {
     $_POST['img']=$_FILES['img']['name'];
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/{$_FILES['img']['name']}");
 }
-/* 用隨機亂數產生商品編號，實務上存檔前應先確認新商品編號在資料表中不重複，
-   才寫入資料表(可用while功能檢查是否重複) */
-$_POST['no']=rand(100000,999999);
 
-// 設定預設為顯示(sh=1)
-$_POST['sh']=1;
+// 若非編輯模式(表新增的狀態)，才產生商品編號及預設為上架
+if (!isset($_POST['id'])) {
+    /* 用隨機亂數產生商品編號，實務上存檔前應先確認新商品編號在資料表中不重複，
+    才寫入資料表(可用while功能檢查是否重複) */
+    $_POST['no']=rand(100000,999999);
+    // 設定預設為顯示(sh=1)
+    $_POST['sh']=1;
+}
 
 $Goods->save($_POST);
 to("../back.php?do=th");
