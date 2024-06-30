3407224d7dfe2b60ebf2b2a99046fc5afe2ccf7d
dean5201314 <smile90624@gmail.com>
2024-06-30 16:00:23 +0800

    site4: 在首頁顯示所有商品資料

--- s4/front/main.php
+++ s4/front/main.php
@@ -44,8 +44,23 @@ if ($type==0) {
 <h2><?=$nav;?></h2>
 
 <?php
+// $goods:商品資料集變數，$good:當前筆商品資料變數
 foreach ($goods as $good) {
-    echo $good['name'];
-    echo "<br>";
+    // 以下2行為測試讀取的資料用
+    // echo $good['name'];
+    // echo "<br>";
+?>
+<div class="item">
+  <div class="img">
+    <img src="./img/<?=$good['img'];?>" style="width: 200px; height: 150px;">
+  </div>
+  <div class="info">
+    <div class="ct tt"><?=$good['name'];?></div>
+    <div>價錢：<?=$good['price'];?></div>
+    <div>規格：<?=$good['spec'];?></div>
+    <div>簡介：<?=$good['intro'];?></div>
+  </div>
+</div>
+<?php
 }
 ?>
