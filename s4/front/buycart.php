9fea5c61d31e106027363d89d91dabcf69eacb7a
dean5201314 <smile90624@gmail.com>
2024-07-02 17:23:50 +0800

    site4: 建立使用session來紀錄購物車的機制

--- s4/front/buycart.php
+++ s4/front/buycart.php
@@ -1,14 +1,19 @@
 <?php
+/* 若點選"我要購買"，則到購物車功能，如果尚未登入($_SESSION['s4_mem']未定義)，重新導向到登入頁面 */
 if (!isset($_SESSION['s4_mem'])) {
     to("?do=login");
+    exit();
 }
-/* 若點選"我要購買"，則到購物車功能，若尚未登入($_SESSION['mem']未定義)，
-   則重新導向到登入頁面 */
 
 echo "<h2 class='ct'>{$_SESSION['s4_mem']}的購物車</h2>";
 
-if (!isset($_GET['id'])) {
+if (!isset($_SESSION['cart'])) {
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
-}
+} else {
+    if (isset($_GET['id'])) {
+        $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+    } 
+        
+    dd($_SESSION['cart']);
 
-?>
+}
