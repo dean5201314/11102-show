6dbcd43e6ba066ec727480ed25274efcf1a4ab0e
dean5201314 <smile90624@gmail.com>
2024-07-03 21:42:15 +0800

    site4: 完成點選我要購買,在購物車中顯示商品

--- s4/front/buycart.php
+++ s4/front/buycart.php
@@ -1,19 +1,67 @@
 <?php
-/* 若點選"我要購買"，則到購物車功能，如果尚未登入($_SESSION['s4_mem']未定義)，重新導向到登入頁面 */
+/* 因應未登入前點了購物車，改為先將點選結果存入 session中，避免登入後，點選結果因未儲存而消失，
+   若有是否收到商品編號，表按下我要購買，就先將商品編號(key)及數量(value)存入購物車矩陣 */
+if (isset($_GET['id'])) {
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+
+/* 若$_SESSION['s4_mem']不存在，表示尚未登入，重新導向到登入頁面 */
 if (!isset($_SESSION['s4_mem'])) {
-    to("?do=login");
+    // 使用 HTML Meta 標籤重定向，因不涉及設置 HTTP標頭，可以避免標頭已經發送的情況下出現錯誤
+    echo '<meta http-equiv="refresh" content="0;url=?do=login">';
+    // 以下的to()採用header()方式重新定向網頁，會因輸出前有HTML輸出(含HTML註解)，而出現修改HTTP標頭(header)
+    // to("?do=login");
     exit();
 }
 
+// 顯示提示訊息：登入者{$_SESSION['s4_mem']}的購物車
 echo "<h2 class='ct'>{$_SESSION['s4_mem']}的購物車</h2>";
 
+// 如沒有$_SESSION購物車矩陣變數，顯示提示訊息：購物車中尚無商品
 if (!isset($_SESSION['cart'])) {
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
-} else {
-    if (isset($_GET['id'])) {
-        $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
-    } 
-        
-    dd($_SESSION['cart']);
-
+}
+// else { dd($_SESSION['cart']); }  使用 dd($_SESSION['cart']); 測試購物車輸入結果
+else 
+{
+?>
+<!-- 顯示表格標題列 -->
+<!-- table.all>tr.ct.tt*2 -->
+<table class="all">
+    <tr class="ct tt">
+        <!-- td*7 -->
+            <td>編號</td>
+            <td>商品名稱</td>
+            <!-- 此處數量是指購物車中輸入的數量 -->
+            <td>數量</td>
+            <td>庫存量</td>
+            <td>單價</td>
+            <!-- 此處小計是指商品單價 * 購物車中輸入的數量 -->
+            <td>小計</td>
+            <td>刪除</td>
+    </tr>
+<?php
+    foreach ($_SESSION['cart'] as $id => $qt) {
+        $goods=$Goods->find($id);
+?>
+        <!-- 顯示表格表身商品資料列 -->
+        <tr class="ct pp">
+            <!-- td*7 -->
+                <td><?=$goods['no'];?></td>
+                <td><?=$goods['name'];?></td>
+                <td><?=$qt;?></td>
+                <td><?=$goods['stock'];?></td>
+                <td><?=$goods['price'];?></td>
+                <td><?=$goods['price'] * $qt;?></td>
+                <td><img src="./icon/0415.jpg" alt="" srcset=""></td>
+        </tr>
+<?php
+    }
+?>
+</table>
+<div class="ct">
+    <img src="./icon/0411.jpg">
+    <img src="./icon/0412.jpg">
+</div>
+<?php
 }
