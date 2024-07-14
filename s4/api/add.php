8486534f6665eaebd4b2500091b1c12467907746
dean5201314 <smile90624@gmail.com>
2024-07-15 03:12:09 +0800

    site4: 建立動態消息後台管理功能

--- s4/api/add.php
+++ s4/api/add.php
@@ -3,10 +3,10 @@ include_once "./db.php";
 
 // 將post方法傳入的table變數內容，首字轉大寫，用可變變數格式${}轉成變數名稱，assign成為變數$DB
 $DB=${ucfirst($_POST['table'])};
-// 將post方法傳入的table變數內容，存為變數$table
+// 將post方法傳入的table變數內容，存為變數$table，供 to() 使用
 $table=$_POST['table'];
 
-// 以下程式碼用途為；DB寫入時可直接使用$_POST變數
+/* 以下為校園網站系統所需程式碼，用途為；DB寫入時可直接使用$_POST變數
 // 若為admin在呼叫，則移除$_POST中資料表不存在的變數pw2
 switch ($table) {
     case 'admin':
@@ -17,12 +17,14 @@ switch ($table) {
 if (isset($_FILES['img']['tmp_name'])) {
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
-}
+}    
+*/
 
-// 非admin呼叫時，判斷除網站標題title外，預設sh欄位為1顯示(因admin資料表沒有sh欄位)
+// 非admin呼叫時，判斷除網站標題title外(校園網站系統所需)，預設sh欄位為1顯示(因admin資料表沒有sh欄位)
 if ($table != 'admin') {
     $_POST['sh']=($table=='title')?0:1;
 }
+
 // 移除$_POST中資料表不存在的變數table
 unset($_POST['table']);
 // $_POST變數經調整後，DB寫入時直接使用$_POST變數
