60b0e833e5e1020f51f6edb251f7b6fe17ea1bdf
dean5201314 <smile90624@gmail.com>
2024-11-08 14:59:00 +0800

    site4: 管理員登入時改用圖形驗證碼功能

--- s4/front/login_old.php
+++ s4/front/login_old.php
@@ -1,4 +1,7 @@
-<h2>管理員登入</h2>
+<h2>第一次購買</h2>
+<img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
+
+<h2>會員登入</h2>
 <!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
 <table class="all">
     <tr>
@@ -14,45 +17,24 @@
         <td class="pp">
             <!-- 如果用js做，前端user可以用F12看到答案，所以改成後端做 -->
             <?php
-            /* 以下為使用加法的驗證程式 
-            // $a=rand(10,99);
-            // $b=rand(10,99);
-            // 用session變
-            // // 用session變數存答案，讓前端看不到" . $b . " = ";
-            */
-
-            /* 以下為使用圖形的驗證程式 
-            $_SESSION['ans'] = code(5);
-            $img=captcha($_SESSION['ans']);  
-            */ 
+            $a = rand(10, 99);
+            $b = rand(10, 99);
+            // 用session變數存答案，讓前端看不到
+            $_SESSION['ans'] = $a + $b;
+            echo $a . " + " . $b . " = ";
             ?>
-            <img src="" id="captcha"><button onclick="captcha()">重新產生</button><br>
             <input type="text" name="ans" id="ans">
         </td>
     </tr>
 </table>
 
-
 <!-- .ct>btn -->
 <div class="ct">
     <!-- login()內要以傳入參數區分資料表來源，故用資料表名稱's4_mem' -->
-    <button onclick="login('s4_admin')">確認</button>
+    <button onclick="login('s4_mem')">確認</button>
 </div>
 
 <script>
-/* 畫面載入時就先產生圖形驗證碼 */
-captcha()
-/* 在chrome測試，captcha元件src屬性已設定為img內容 */
-// let gblimg;  //依 chatgpt建議設定global變數
-function captcha(){  
-    $.get("./api/captcha.php",(img)=>{
-        // gblimg=img;  //將內部變數 img 傳給 global變數 gblimg
-        // console.log(gblimg); // 打印 gblimg 的值
-        // console.log(img);    // 打印 img 的值
-        $("#captcha").attr("src",img)
-    })
-}
-
 function login(table) {
     /* 測試ajax:顯示php程式執行前的參數值
     let ans=$("#ans").val();
@@ -62,7 +44,7 @@ function login(table) {
     $.get('./api/chk_ans.php',{ans:$("#ans").val()}, (chk) => {
         // 實務上若驗證碼錯誤，會重新產生驗證碼，再要求重新輸入
         if (parseInt(chk) == 0) {
-            alert("對不起，您輸入的驗證碼有誤，請您重新輸入")
+            alert("驗證碼錯誤，請重新輸入")
         } else {
             // 若驗證碼正確，則檢查帳號與密碼是否正確
             $.post('./api/chk_pw.php',{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
@@ -76,7 +58,7 @@ function login(table) {
                     alert("帳號或密碼錯誤，請重新輸入")
                 } else {
                     // 若帳號密碼正確，就回到首頁
-                    location.href='back.php?do=admin'
+                    location.href='index.php'
                 }
             })
         }
